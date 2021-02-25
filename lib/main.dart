9c34845f9c9512289466fa195bfe2ee85ebaad10
alexDollard <alexdollard141@gmail.com>
2021-02-25 13:37:52 -0600

    loaded biz card from Angela tutorial to play with later

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan[800],
+        body: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: AssetImage('images/alex.jpeg'),
+                ),
+                Text(
+                    'Alex Dollard',
+                    style: TextStyle(
+                      fontFamily: 'Satisfy',
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w500,
+                    )
+                ),
+                Text(
+                    'Flutter Developer'.toUpperCase(),
+                    style: TextStyle(
+                      fontFamily: 'Questrial',
+                      color: Colors.cyan[100],
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 2.5,
+                    )
+                ),
+                SizedBox(
+                    height: 20.0,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.cyan[100],
+                    )
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.cyan[900],
+                    ),
+                    title: Text(
+                      '770-542-9231',
+                      style: TextStyle(
+                          color: Colors.cyan[900],
+                          fontSize: 18.0
+                      ),
+                    ),
+                  ),
+                ),
+
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                        leading: Icon(
+                          Icons.email,
+                          color: Colors.cyan[900],
+                        ),
+                        title: Text(
+                            'alexdollard141@gmail.com',
+                            style: TextStyle(
+                              color: Colors.cyan[900],
+                              fontSize: 18.0,
+                            )
+                        )
+                    )
+                ),
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+}
